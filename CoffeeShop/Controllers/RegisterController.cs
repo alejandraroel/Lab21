4f7bb7afdd4783c721390aba3d24acd7accc200a
Your Name <alejandraroel@Alejandras-MacBook-Air-2.local>
2020-04-14 19:04:24 -0400

    first commit

---
+++ CoffeeShop/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoffeeShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoffeeShop.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var model = new RegistrationViewModel();
+            return View("Register Index", model);
+        }
+
+        public IActionResult Registration(RegistrationViewModel model)
+        {
+            
+            return View(model);
+        }
+    }
+}
---
+++ CoffeeShop/Models/RegistrationViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CoffeeShop.Models
+{
+    public class RegistrationViewModel
+    {
+        public RegistrationViewModel()
+        {
+        }
+
+        public RegistrationViewModel(string username, string email, string password)
+        {
+            UserName = username;
+            Email = email;
+            Password = password;
+        }
+
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+}
+
+
